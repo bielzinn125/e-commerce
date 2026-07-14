156503d1118fb050544d9a03e4dc3617235116fb
Gabriel vaz <joao.g.oliveira125@aluno.senai.br>
2026-07-13 21:33:51 -0300

    cria novas variáveis de preço e produtos

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
-
+import { UpperCasePipe } from '@angular/common';
 @Component({
   selector: 'app-produto',
-  imports: [],
+  imports: [UpperCasePipe],
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
-export class Produto {}
+//Adiciona variaveis e condições do produto
+export class Produto {
+  produto = 'Notebook Gamer';
+  preco = 7099.99; //preco = preço
+  mostrarProduto = true;
+  mostrarPreco = true;
+  produtos =[
+    {nome: 'Teclado' , preco:45.50},
+    {nome: 'Mouse' , preco:36.70},
+    {nome: 'Monitor' , preco:980.00}
+  ];
+}
