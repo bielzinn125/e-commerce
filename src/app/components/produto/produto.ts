70a173a65559d90de8c7aad4366e5aab5123ad30
Gabriel vaz <joao.g.oliveira125@aluno.senai.br>
2026-07-15 19:03:27 -0300

    feat: criar componente Produto com propriedades de entrada

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,20 +1,15 @@
-import { Component } from '@angular/core';
-import { UpperCasePipe } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { UpperCasePipe, CurrencyPipe} from '@angular/common';
+//O IMPORT SERVE PRA TRAZER CARACTERIZAÇOES DE TEXTO (NUMERO, LETRA, ETC)
+import {PrecoFormatadoPipe} from '../../pipes/preco-formatado-pipe'
 @Component({
   selector: 'app-produto',
-  imports: [UpperCasePipe],
+  imports: [UpperCasePipe, PrecoFormatadoPipe  ],
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
 //Adiciona variaveis e condições do produto
 export class Produto {
-  produto = 'Notebook Gamer';
-  preco = 7099.99; //preco = preço
-  mostrarProduto = true;
-  mostrarPreco = true;
-  produtos =[
-    {nome: 'Teclado' , preco:45.50},
-    {nome: 'Mouse' , preco:36.70},
-    {nome: 'Monitor' , preco:980.00}
-  ];
+  @Input() nome: string = '';
+  @Input() preco: number = 0;
 }
