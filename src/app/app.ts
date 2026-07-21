436ca4683346c7ed7671873a9b94710deb4b495f
Gabriel vaz <joao.g.oliveira125@aluno.senai.br>
2026-07-21 19:53:38 -0300

    feat: importa rotas e links para app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
-//import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 //import { Produto } from './components/produto/produto';
-import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
+import { UpperCasePipe } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [ListaProdutos],
+  imports: [RouterOutlet, RouterLink, UpperCasePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
+
+  nomeLoja = 'cyber engenharia';
 }
