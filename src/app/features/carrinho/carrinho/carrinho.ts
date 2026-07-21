149ebbfcf025295dba1c5442873c1db9498387da
Gabrielly Amaral <gabyamaral2805@gmail.com>
2026-07-21 18:33:23 -0300

    feat: cria novo componente carrinho

---
+++ src/app/features/carrinho/carrinho/carrinho.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrinho',
+  imports: [],
+  templateUrl: './carrinho.html',
+  styleUrl: './carrinho.css',
+})
+export class Carrinho {
+  //!LOGICA DE CARRINHO VIRÁ NAS PROXIMAS AULAS
+  //!POR HORA, APENAS EXIBE A PÁGINA CARRINHO
+}
