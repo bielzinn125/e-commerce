3daa722ca278112d63c7d14748cffc01ac719e40
Gabriel vaz <joao.g.oliveira125@aluno.senai.br>
2026-07-22 21:32:01 -0300

    feat: cria arquivo auth.ts e authGuard.ts

---
+++ src/app/core/auth.guard.ts
@@ -0,0 +1,13 @@
+import { CanActivateFn } from "@angular/router";
+import { usuarioLogado} from "./auth";
+
+export const authGuard: CanActivateFn = () => {
+    return usuarioLogado();
+
+
+
+
+
+
+
+}
