c70835c6c318d5ade2a406c877a79f2a3119e1ff
Gabriel vaz <joao.g.oliveira125@aluno.senai.br>
2026-08-07 21:13:28 -0300

    feat: exibir quantidade de itens do carrinho

--- src/app/shared/layout/header/header.ts
+++ src/app/shared/layout/header/header.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {RouterLink} from '@angular/router';
-
+import { CarrinhoService } from '../../../core/services/carrinho.service';
+import { inject } from '@angular/core';
 @Component({
   selector: 'app-header',
   imports: [MatToolbarModule, MatButtonModule, RouterLink],
@@ -11,4 +12,10 @@ import {RouterLink} from '@angular/router';
 })
 export class Header {
   nomeLoja = 'Cyber Engenharia';
+  
+  private carrinhoService = inject(CarrinhoService);
+  
+  quantidadeHeader = this.carrinhoService.quantidadeItens;
+
+
 }
