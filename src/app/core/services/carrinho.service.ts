a40a9537ab5004da3a2bfae87ab420f7c55a908b
Gabriel vaz <joao.g.oliveira125@aluno.senai.br>
2026-08-12 20:13:55 -0300

    refactor: estado global do carrinho validando condição de mais de um item

--- src/app/core/services/carrinho.service.ts
+++ src/app/core/services/carrinho.service.ts
@@ -2,12 +2,16 @@ import { Injectable } from "@angular/core";
 import { signal } from "@angular/core";
 import { computed } from "@angular/core";
 
+type ItemCarrinho = {
+    nome: string;
+    preco: number;
+}
 @Injectable({providedIn:  'root'
 })
 
 export class CarrinhoService {
 // !Estado global
-private carrinho = signal<{nome: string; preco:number}[]>([]);
+private carrinho = signal<ItemCarrinho[]>([]);
 
 //? Seleção
 itens = computed(() => this.carrinho());
@@ -16,8 +20,10 @@ quantidadeItens = computed(() => this.carrinho ().length);
 totalItens = computed (() => 
 this.carrinho().reduce((total, item ) => total + item.preco,0));
 
+carrinhoVazio = computed(() => this.carrinho().length === 0);
+
 // TODO: ações Adicionar Produtos
-adicionar(produto: {nome: string; preco: number;}){
+adiciona(produto: ItemCarrinho) {
     this.carrinho.update (lista => [
     ...lista, produto]);
 }
