8ace421752ab8bc16f64cd3e486ba4fd27a57b53
Gabriel vaz <joao.g.oliveira125@aluno.senai.br>
2026-08-13 19:06:32 -0300

    refactor(http): integra interceptor ao AuthService

--- src/app/core/interceptors/http.interceptor.ts
+++ src/app/core/interceptors/http.interceptor.ts
@@ -1,33 +1,36 @@
-import { HttpInterceptorFn } from "@angular/common/http";
-import { tap } from "rxjs";
-import { catchError } from "rxjs";
-import { throwError } from "rxjs";
+import { inject } from '@angular/core';
+import { HttpInterceptorFn } from '@angular/common/http';
+import { tap, catchError, throwError } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
-export const httpInterceptor: HttpInterceptorFn = (req,next) => {
-    console.log('Interceptando Requisição:' , req.url);
-    //! aqui voce vai adicionar um token pra modificar requisição
-    const token = 'fake-token-jwt';
-
-    const novaReq = req.clone({
-        setHeaders: {
-            Authorization: `Bearer ${token}`, 
-        },
-    });
-    return next(novaReq).pipe(
-        tap({
-            next: (event) => console.log ('Responde: ', event),
-            error: (error) => console.error ('Erro de Requisição', error),
-        }),
-
-        catchError((error) => {
-            console.error('ERRO GLOBAL: ', error);
-            if (error.status === 401) {
-                console.warn('Usuário não autorizado!', error);
-            }
-            if (error.status === 500) {
-                console.warn('Erro interno do serviço', error);
-            }
-            return throwError(() => error);
-        }),
-    );
+export const httpInterceptor: HttpInterceptorFn = (req, next) => {
+const authService = inject(AuthService);
+const token = authService.obterToken();
+//! LOG REQUEST
+console.log('REQUEST', req.url);
+//! TOKEN
+const novaReq = token
+? req.clone({
+setHeaders: {
+Authorization: `Bearer ${token}`,
+},
+})
+: req;
+//! SEGUE COM A NOVA REQUEST + LOG RESPONSE
+return next(novaReq).pipe(
+tap({
+next: (event) => console.log('RESPONSE:', event),
+error: (error) => console.error('ERRO:', error),
+}),
+catchError((error) => {
+console.error('ERRO GLOBAL:', error);
+if (error.status === 401) {
+console.warn('Não autorizado!');
+}
+if (error.status === 500) {
+console.warn('Erro interno do servidor!');
+}
+return throwError(() => error);
+}),
+);
 };
