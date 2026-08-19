cae899c188b5cd1c743aaf807d9cf976454685b2
Gabriel vaz <joao.g.oliveira125@aluno.senai.br>
2026-08-19 18:52:15 -0300

    refactor(admin): adiciona funções de administrador ao projeto

--- src/app/features/admin/admin/admin.ts
+++ src/app/features/admin/admin/admin.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import { computed, signal, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+import { MatAnchor } from "@angular/material/button";
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [MatAnchor],
   templateUrl: './admin.html',
   styleUrl: './admin.css',
 })
-export class Admin {}
+export class Admin {
+  private router = inject(Router);
+  private authService = inject(AuthService);
+
+  totalProdutosCadastrados = signal(30);
+  pedidosPendentes = signal(6);
+  usuariosCadastrados = signal(7);
+
+  usuarioAtual = this.authService.usuarioAtual;
+
+  areaPerfil = computed(() =>{
+    const usuario = this.usuarioAtual();
+
+    if(!usuario){
+      return `Nenhum usuário autenticado`
+    }
+    return `Usuário autenticado como: ${usuario.perfil}`;
+  });
+
+  sair(){
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
+  }
+
+}
